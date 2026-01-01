a90ec0015e0af22d272777428ddb9f25e0dbcbee
harsh-suman5 <harshsuman457@gmail.com>
2026-01-01 23:01:19 +0530

    initial commit

---
+++ new.py
@@ -0,0 +1,31 @@
+# conditions like if, elif, else, 
+# loops like for and while,
+#break and continue statements,
+# function definitions using def,
+# and exception handling using try, except, finally.
+# a = int(input("Enter value for a: "))
+# b = int(input("Enter value for b: "))
+# if a>b:
+#     print("a is greater than b")
+# else:
+#     print("b is greater than a")
+
+# a = int(input("Enter value for a: "))
+# if a%2==0:
+#     print("a is even")
+# elif a%2!=0:
+#     print("a is odd")
+# else:
+#     print("Invalid input")
+
+#loops
+# for loop and while loop
+for i in range(1,11):
+    print(i)
+
+
+
+
+
+
+
