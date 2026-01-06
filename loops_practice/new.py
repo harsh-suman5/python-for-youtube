bd153ab93648f5a37676d2d3ba5efcaf0db42229
harsh-suman5 <harshsuman457@gmail.com>
2026-01-06 21:54:06 +0530

    for loop file is added

---
+++ loops_practice/new.py
@@ -0,0 +1,13 @@
+#starting with for loops
+#loops:-- to repeat a block of code multiple times
+#in python we have two types of loops: for loop and while loop
+#print  bet 1 to 10
+students = ['harsh','rahul','sonam','anita']
+for a in students:
+    print(a)
+    if a == "harsh":
+        print("result found✅")
+        break
+    else:
+        print("result not found❌")
+
