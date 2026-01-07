0e970fd2cad27192fdf3c7b600a87a610399466a
harsh-suman5 <harshsuman457@gmail.com>
2026-01-07 22:28:25 +0530

    functions lecture started

---
+++ functions/new.py
@@ -0,0 +1,11 @@
+#functions: it is a block of code that gives result onlyn when it is called
+#why functions are used: 1.code reusablity 2. modularity 3. readability 4. maintainablity
+#defining a function: to define a function we use def keyword followed by function name
+
+# example are: 
+
+def welcome():
+    print("welcom to the lecture of functions by harsh sir.") # -> this is the syntax of defining a function
+
+# calling the function
+welcome()
